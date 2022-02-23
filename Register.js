45ab2828f48dd144fa5f0e57219981954252546e
Seif <ben.houidi.seifeddine@hotmail.com>
2022-02-23 17:24:47 +0100

    Fonction Ajout Local Storage + Converssion Objet en Tableau avec la fonction JSON.parse

---
+++ Register.js
@@ -0,0 +1,23 @@
+let array=[];
+function ajout(){
+  let email = document.getElementById("email").value;
+  let edress = document.getElementById("adress").value;
+  let adress2 = document.getElementById("adress2").value;
+  let city = document.getElementById("city").value;
+  let state = document.getElementById("state").value;
+  let zip = document.getElementById("zip").value;
+
+  
+
+
+
+  let user={ Email:email , Password:password ,  Adress : adress ,  Adress2 : adress2 , City : city , State : state , Zip : zip }
+  // console.log(user);
+  array.push(user);
+  console.log(array);
+  localStorage.setItem("users",JSON.stringify(array));
+ let users = localStorage.getItem("users");
+  console.log(users);
+
+
+}
--- js/contact.js
+++ js/contact.js
@@ -8,16 +8,6 @@
         let YMessage = document.getElementById("ymessage").value;
         
       
-        console.log("fName : ", fName);
-        console.log("lName : ", lName);
-        console.log("email : ", email);
-        console.log("uName : ", uName);
-        console.log("city : ", city);
-        console.log("state : ", state);
-        console.log("zip : ", zip);
-        console.log("check : ", check);
-        console.log("pw1 : ", pw1);
-        console.log("pw2 : ", pw2);
       
         let user={ Name:name ,Subject: subject,Email:email, Phone : phone   , Message:message , YMessage:message}
         // console.log(user);
